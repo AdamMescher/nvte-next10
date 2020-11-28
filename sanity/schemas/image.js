a2a2a95db4ed138d6ccbfa6b87ef6e8f42f95e7f
Adam Mescher <admescher@gmail.com>
2020-11-28 15:46:55 -0700

    Update schema with image and video types

---
+++ sanity/schemas/image.js
@@ -0,0 +1,39 @@
+export default {
+  name: 'staticImage',
+  title: 'Image',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'name',
+      title: 'Image Name',
+      type: 'string',
+    },
+    {
+      name: 'altText',
+      title: 'Alt Text',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'keywords',
+      type: 'array',
+      title: 'Keywords',
+      of: [{ type: 'string' }],
+      options: {
+        layout: 'tags',
+      },
+    },
+  ],
+};
---
+++ sanity/schemas/video.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'video',
+  title: 'Video',
+  type: 'document',
+  fields: [
+    {
+      name: 'video',
+      title: 'Video',
+      type: 'file',
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+  ],
+};
